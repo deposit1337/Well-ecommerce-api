ae8b753bfa652d24d2d72d84437055b9ab112f98
depo_ <propopa228@mail.ru>
2024-07-14 00:44:07 +0800

    init: update models and DB

---
+++ main/catalog/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Brand,Category,Item,AdditionalPicture
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class AdditionalPictureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdditionalPicture
+        fields = '__all__'
--- main/catalog/views.py
+++ main/catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Item
+from .serializers import ItemSerializer
+
+
+class ItemView(viewsets.ViewSet):
+    # Class based viewset to all items
+
+    queryset = Item.objects.all
+
+    def list(self, request):
+        serializer = ItemSerializer(self.queryset, many=True)
+        return Response(serializer.data)
